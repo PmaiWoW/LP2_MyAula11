0d04a60c3b5a4a1ae42e71f6bb2c629df0e45957
PmaiWoW <PmaiWoW@Gmail.com>
2019-12-18 16:18:57 +0000

    First commit, with exercise 1 from class 11 (DoubleBuffer2D<T>)

---
+++ DoubleBuffer2D/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DoubleBuffer2D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            DoubleBuffer2D<char> db =
+                new DoubleBuffer2D<char>(5, 5);
+
+            db[1, 0] = 'x';
+            db[2, 0] = 'x';
+            db[3, 0] = 'x';
+            db[4, 0] = 'x';
+            db[1, 1] = 'x';
+            db[1, 2] = 'x';
+            db[2, 2] = 'x';
+            db[3, 2] = 'x';
+            db[3, 3] = 'x';
+            db[0, 4] = 'x';
+            db[1, 4] = 'x';
+            db[2, 4] = 'x';
+            db[3, 4] = 'x';
+            
+            db.Swap();
+            db.Render();
+        }
+    }
+}
